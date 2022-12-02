752ea0bd22e070b8c06433aed50d406f6fdf378a
Huy Duong <huy.duongdinh@gmail.com>
2022-12-02 18:53:34 +0700

    Day 1: Calorie Counting [Part 2]

--- 2022/day1/day1.cpp
+++ 2022/day1/day1.cpp
@@ -1,25 +1,49 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <queue>
 #include <string>
 
 int main() {
   std::ifstream inputFile("input.txt");
 
   std::string calorie;
-  int maxCalories = 0;
   int currentCalories = 0;
+  // min heap
+  std::priority_queue<int, std::vector<int>, std::greater<int>> caloriesQueue;
 
   while (std::getline(inputFile, calorie)) {
     if (calorie == "") {
-      maxCalories = std::max(maxCalories, currentCalories);
+      if (caloriesQueue.size() < 3) {
+        caloriesQueue.push(currentCalories);
+      } else if (caloriesQueue.top() < currentCalories) {
+        caloriesQueue.pop();
+        caloriesQueue.push(currentCalories);
+      }
+
       currentCalories = 0;
     } else {
       currentCalories += std::stoi(calorie);
     }
   }
 
-  maxCalories = std::max(maxCalories, currentCalories);
+  if (caloriesQueue.size() < 3) {
+    caloriesQueue.push(currentCalories);
+  } else if (caloriesQueue.top() < currentCalories) {
+    caloriesQueue.pop();
+    caloriesQueue.push(currentCalories);
+  }
+
+  int totalTopThreeCalories = 0;
+  int maxCalories = 0;
+
+  while (!caloriesQueue.empty()) {
+    totalTopThreeCalories += caloriesQueue.top();
+    maxCalories = std::max(maxCalories, caloriesQueue.top());
+    caloriesQueue.pop();
+  }
+
   std::cout << maxCalories << std::endl;
+  std::cout << totalTopThreeCalories << std::endl;
 
   inputFile.close();
 }
